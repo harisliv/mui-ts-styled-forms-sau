972dfdb53c48d90f4223a21b0d4ff30a9ad7880d
Harisliv <livieratos.theoxaris@gmail.com>
2022-01-14 00:47:19 +0200

    Login form styled mui v1

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,36 @@ import { createTheme, ThemeProvider } from "@mui/material";
 import * as React from "react";
 import BasicGrid from "./components/BasicGrid";
 import MaterialForm from "./components/MaterialForm";
+import { CssBaseline, GlobalStyles } from "@mui/material";
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    neutral: Palette['primary'];
+  }
+
+  interface PaletteOptions {
+    neutral?: PaletteOptions['primary'];
+  }
+}
 
 const theme = createTheme({
   palette: {
     primary: {
       main: "#DE1515",
     },
+    neutral: {
+      main: '#EDEDED',
+      contrastText: '#000000',
+    },
+    background: {
+      default: "#000",
+    },
   },
   typography: {
-    fontFamily: "Inter",
+    fontFamily: ["Inter", "Playfair Display"].join(','),
     fontWeightLight: 400,
-    fontWeightRegular: 700,
-    fontWeightMedium: 900,
+    fontWeightRegular: 500,
+    fontWeightMedium: 700,
   },
   spacing: 18,
 });
@@ -21,6 +39,12 @@ const theme = createTheme({
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <GlobalStyles
+        styles={{
+          body: { backgroundColor: "#f9f9f9" },
+        }}
+      />
       <BasicGrid>
         <MaterialForm />
       </BasicGrid>
--- src/components/MaterialForm.tsx
+++ src/components/MaterialForm.tsx
@@ -1,24 +1,95 @@
-import { useFormik } from 'formik';
-import * as yup from 'yup';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import { useFormik } from "formik";
+import * as yup from "yup";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { styled } from "@mui/material/styles";
+import InputAdornment from "@mui/material/InputAdornment";
+import InputLabel from "@mui/material/InputLabel";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+
+declare module '@mui/material/Button' {
+  interface ButtonPropsColorOverrides {
+    neutral: true;
+  }
+}
+
+const ForgotPassword = styled(Typography)`
+  && {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 24px;
+    color: #de1515;
+  }
+`;
+const StyledTypography = styled(Typography)`
+  && {
+    font-family: Playfair Display;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 32px;
+    letter-spacing: 0em;
+    text-align: left;
+  }
+`;
+
+const StyledTextField = styled(TextField)`
+  fieldset {
+    border-radius: 8px;
+    border: 1px solid #0000001f;
+    transition: border 0.3s;
+  }
+  && {
+    background-color: #fff;
+    border-radius: 8px;
+  }
+
+  input:hover {
+    color: #2e8b57;
+  }
+`;
+
+const StyledButton = styled(Button)`
+  && {
+    text-transform: none;
+    border-radius: 8px;
+    box-shadow: none;
+  }
+`;
+
+const StyledInputLabel = styled(InputLabel)`
+  &&& {
+    font-family: Inter;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 24px;
+    letter-spacing: 0.25px;
+    text-align: left;
+    color: #000000de;
+  }
+`;
 
 const validationSchema = yup.object({
   email: yup
     .string()
-    .email('Enter a valid email')
-    .required('Email is required'),
+    .email("Enter a valid email")
+    .required("Email is required"),
   password: yup
     .string()
-    .min(8, 'Password should be of minimum 8 characters length')
-    .required('Password is required'),
+    .min(8, "Password should be of minimum 8 characters length")
+    .required("Password is required"),
 });
 
 const MaterialForm = () => {
   const formik = useFormik({
     initialValues: {
-      email: 'foobar@example.com',
-      password: 'foobar',
+      email: "",
+      password: "",
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
@@ -28,33 +99,76 @@ const MaterialForm = () => {
 
   return (
     <div>
+      <StyledTypography variant="h3">Welcome back</StyledTypography>
+      <Typography variant="caption">
+        Log in using your account details
+      </Typography>
+
       <form onSubmit={formik.handleSubmit}>
-        <TextField
+        <StyledInputLabel sx={{ mt: 0.85 }} htmlFor="email">
+          Email
+        </StyledInputLabel>
+        <StyledTextField
           fullWidth
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AlternateEmailIcon sx={{ color: "#000" }} />
+              </InputAdornment>
+            ),
+          }}
           id="email"
           name="email"
-          label="Email"
+          type="email"
           value={formik.values.email}
           onChange={formik.handleChange}
           error={formik.touched.email && Boolean(formik.errors.email)}
           helperText={formik.touched.email && formik.errors.email}
-          sx={{mt: 2}}
+          sx={{ mt: 0.01 }}
         />
-        <TextField
+
+        <StyledInputLabel sx={{ mt: 0.85 }} htmlFor="password">
+          Password
+        </StyledInputLabel>
+        <StyledTextField
           fullWidth
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <LockOutlinedIcon sx={{ color: "#000" }} />
+              </InputAdornment>
+            ),
+          }}
           id="password"
           name="password"
-          label="Password"
           type="password"
           value={formik.values.password}
           onChange={formik.handleChange}
           error={formik.touched.password && Boolean(formik.errors.password)}
           helperText={formik.touched.password && formik.errors.password}
-          sx={{mt: 2}}
+          sx={{ mt: 0.01 }}
         />
-        <Button sx={{mt: 2}} color="primary" variant="contained" fullWidth type="submit">
-          Submit
-        </Button>
+      <ForgotPassword variant="caption">Forgot password</ForgotPassword>
+
+      <StyledButton
+          sx={{ mt: 0.85, p: 0.7 }}
+          color="primary"
+          variant="contained"
+          fullWidth
+          type="submit"
+        >
+          Log in
+        </StyledButton>
+
+        <StyledButton
+          sx={{ mt: 0.85, p: 0.7 }}
+          color="neutral"
+          variant="contained"
+          fullWidth
+          type="submit"
+        >
+          Create account
+        </StyledButton>
       </form>
     </div>
   );
